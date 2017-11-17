e0ca700f8117544a6bf546f80d71df06c95be79b
Admin <ekkawit.lertkrai@gmail.com>
2017-11-17 11:22:24 +0700

    bug fixes generation token and encap password

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,22 @@
 import express from 'express'
 import request from 'request'
+import md5 from 'md5'
 import connection from '../database'
 import { API_SERVER } from '../config'
 
 const router = express.Router()
 
 router.post('/:citizen_id/accessTokens', async function (req, res) {
-  const data = await getAccessToken(req.body.username, req.body.password)
-  res.status(200).send(data);
+  const data = await getAccessToken(req.body.username, md5(req.body.password))
+  if (data) {
+    require('crypto').randomBytes(48, async function (err, buffer) {
+      const token = buffer.toString('hex')
+      if (await setAccessToken(token, data.accessToken)) {
+        const result = await getCitizenId(token)
+        res.status(200).send(result)
+      }
+    })
+  }
 })
 
 router.get('/:accessToken/citizenId', async function (req, res) {
@@ -41,6 +50,23 @@ const getCitizenId = async (accessToken) => {
           resolve(rows[0])
         } else {
           reject(err)
+        }
+      }
+    )
+  })
+}
+
+const setAccessToken = async (accessToken, oldAccessToken) => {
+  return new Promise(function (resolve, reject) {
+    connection.query(
+      `UPDATE auth
+      SET accessToken = '${accessToken}'
+      WHERE accessToken = '${oldAccessToken}' `,
+      function (err, rows, fields) {
+        if (!err) {
+          resolve(true)
+        } else {
+          resolve(false)
         }
       }
     )
